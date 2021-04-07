77d77d72efece5166ec254e527214fbaff5de744
Hasan Çobanoğlu <hasan.cobanoglu1@outlook.com.tr>
2021-04-07 08:00:58 +0300

    use hooks and func cart comp

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext, useMemo } from 'react';
 import {CardItemDiv} from '../../container/ItemDetails/ItemDetailsStyles';
 import { Link } from 'react-router-dom';
 import { DataConText } from '../../container/context/Context';
@@ -6,53 +6,52 @@ import {CardLink, StyledH2, StyledSpan } from '../featuredProducts/featuredProdu
 import Colors from '../colorsComponent/Colors';
 import { CountButton, TotalDiv, CountSpan, DeleteButton } from './cartStyle';
 
-export default class Cart extends Component {
-    static contextType = DataConText;
+const Cart = () => {
+    const context = useContext(DataConText);
+    const { cart, totalPrice} = context.state;
+    const { reduceAmount, increaseAmount, removeProduct, calculateTotalPrice} = context;
 
-    componentDidMount() {
-        this.context.calculateTotalPrice();
-    }
+    useMemo(() => calculateTotalPrice(),[calculateTotalPrice]);
 
-    render() {
-        const { cart, totalPrice } = this.context.state;
-        const { reduceAmount, increaseAmount, removeProduct} = this.context;
-
-         if(cart.length===0) {
-           return <StyledH2 style={{textAlign:"center"}}>No product!</StyledH2>
-        } else {
-            return ( 
-                <>
-                    {
-                        cart.map(item => (
-                        <CardItemDiv key={item.id}>
-                        <img src={item.src} alt={item.title} />
-                        <>
-                        <div>
-                            <h3>
-                            <CardLink to={`/${item.id}`}>
-                                {item.title}
-                            </CardLink>
-                            </h3>
-                            <StyledSpan >₺{item.price * item.count}</StyledSpan>
-                        </div>
-                        <Colors colors={item.colors}/>
-                        <p>Morbi in ligula lacus. Cras feugiat lacus ipsum, quis gravida elit commodo ut. Ut posuere fermentum elit, eget aliquet ante consectetur quis. Quisque magna ipsum, molestie ac elit eu, tempor dictum urna.</p>
-                        <>
-                            <CountButton onClick={()=>reduceAmount(item.id)}>-</CountButton>
-                            <CountSpan>{item.count}</CountSpan>
-                            <CountButton onClick={()=>increaseAmount(item.id)}>+</CountButton>
-                        </>
-                        </>
-                        <DeleteButton onClick={()=>removeProduct(item.id)}>X</DeleteButton>
-                    </CardItemDiv>
-                    ))
-                    }
-                    <TotalDiv>
-                    <Link to="/payment">Payment</Link>
-                    <h3>Total:₺ {totalPrice}</h3>
-                   </TotalDiv>
-                </>
-                )
+    if(cart.length===0) {
+        return <StyledH2 style={{textAlign:"center"}}>No product!</StyledH2>
+     } else {
+         return ( 
+         <>
+         {
+         cart.map(item => (
+        <CardItemDiv key={item.id}>
+            <img src={item.src} alt={item.title} />
+            <>
+            <div>
+                <h3>
+                    <CardLink to={`/${item.id}`}>
+                        {item.title}
+                    </CardLink>
+                </h3>
+                <StyledSpan >₺{item.price * item.count}</StyledSpan>
+            </div>
+            <Colors colors={item.colors}/>
+            <p>Morbi in ligula lacus. Cras feugiat lacus ipsum, quis gravida elit commodo ut. Ut posuere fermentum elit, eget aliquet ante consectetur quis. Quisque magna ipsum, molestie ac elit eu, tempor dictum urna.</p>
+            <>
+            <CountButton onClick={()=>reduceAmount(item.id)}>-</CountButton>
+            <CountSpan>{item.count}</CountSpan>
+            <CountButton onClick={()=>increaseAmount(item.id)}>+</CountButton>
+            </>
+            </>
+            <DeleteButton onClick={()=>removeProduct(item.id)}>X</DeleteButton>
+        </CardItemDiv>
+        ))
         }
+        <TotalDiv>
+            <Link to="/payment">Payment</Link>
+            <h3>Total:₺ {totalPrice}</h3>
+            </TotalDiv>
+        </>
+        )
     }
-}
+
+
+};
+
+export default Cart;
